777f427e708ac60ab7e4c69d95e0f12685b46cea
aleh-god <aleh.god@gmail.com>
2022-12-24 14:45:16 +0300

    add: Project files.

---
+++ app/src/main/java/by/godevelopment/binlist/ui/theme/Color.kt
@@ -0,0 +1,19 @@
+package by.godevelopment.binlist.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val primaryColor = Color(0xFF781c68)
+val primaryLightColor = Color(0xFFa94d96)
+val primaryDarkColor = Color(0xFF49003d)
+val secondaryColor = Color(0xFF319da0)
+val secondaryLightColor = Color(0xFF6aced1)
+val secondaryDarkColor = Color(0xFF006e72)
+val surfaceColor = Color(0xFFFBE8D3)
+
+val primaryTextColor = Color(0xFFffffff)
+val secondaryTextColor = Color(0xFF000000)
